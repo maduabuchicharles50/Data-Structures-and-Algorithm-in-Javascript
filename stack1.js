91a73cbda52fd6e9e4dcac2b89e48110791cb23c
Charles Mbonu <112169394+maduabuchicharles50@users.noreply.github.com>
2023-01-18 05:42:01 +0100

    Create  stack1.js
    implementation of basic stack data structure

---
+++ stack1.js
@@ -0,0 +1,11 @@
+// adding element to a stack
+let stack = [];
+
+stack.push("orange");
+stack.push("mango");
+stack.push("apple");
+stack.push("cashew");
+stack.push("guava");
+stack.pop()
+console.log(stack);
+// LIFO
